eafc2365d7717778d3467e307ae1e4be79f6eb7f
akihiro.tanaka <big.a.0816@gmail.com>
2021-11-26 00:48:30 +0900

    fix to be interactive cli

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,21 @@
 import { config } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
+import Ask from "https://deno.land/x/ask@1.0.6/mod.ts";
 import { ethers } from "https://esm.sh/ethers?dts";
 
 import { sendLineNotify } from "./utils/line-helper.ts";
 
 const { INFURA_URL, CONTRACT_ADDRESS } = config();
+
+const ask = new Ask();
+
+const answer = await ask.prompt([
+  {
+    name: "address",
+    type: "input",
+    message: "What's target address?",
+    validate: (val) => val === "" || !isNaN(parseInt(String(val))),
+  },
+]);
 
 const provider = new ethers.providers.WebSocketProvider(INFURA_URL);
 const blockNumber = await provider.getBlockNumber();
@@ -15,6 +27,15 @@ const abi = [
   "event PunkBought(uint indexed punkIndex, uint value, address indexed fromAddress, address indexed toAddress)",
   "event PunkOffered(uint indexed punkIndex, uint minValue, address indexed toAddress)",
 ];
+
+const getAddressName = async (address: string): Promise<string> => {
+  const ensName = await provider.lookupAddress(address);
+  return ensName || address;
+};
+
+const address = String(answer.address);
+const target = address && (await getAddressName(address));
+console.log("target:", target || "none");
 
 const sendNotify = async (
   text: string,
@@ -33,19 +54,21 @@ const sendNotify = async (
 
 const erc721 = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
 
-erc721.on("PunkTransfer", (owner, to, punkIndex, { transactionHash }) => {
-  const msg = `PunkTransfer: ${punkIndex} was transferd from ${owner} to ${to}.`;
-  sendNotify(msg, transactionHash, punkIndex);
+erc721.on("PunkTransfer", async (from, to, punkIndex, { transactionHash }) => {
+  const owner = await getAddressName(from);
+  const reciever = await getAddressName(to);
+  const msg = `PunkTransfer: ${punkIndex} was transferd from ${owner} to ${reciever}.`;
+  if (!target || target.toLocaleLowerCase() === owner.toLocaleLowerCase()) {
+    sendNotify(msg, transactionHash, punkIndex);
+  }
 });
 
-erc721.on("PunkBought", (punkIndex, value, owner, to, { transactionHash }) => {
-  const etherStr = ethers.utils.formatEther(value);
-  const msg = `PunkBought: ${punkIndex} was bought from ${owner} to ${to} for ${etherStr} ether.`;
-  sendNotify(msg, transactionHash, punkIndex);
-});
-
-erc721.on("PunkOffered", (punkIndex, minValue, to, { transactionHash }) => {
-  const etherStr = ethers.utils.formatEther(minValue);
-  const msg = `PunkOffered: ${punkIndex} was offerd from ${to} for ${etherStr} ether.`;
-  sendNotify(msg, transactionHash, punkIndex);
-});
+erc721.on(
+  "PunkOffered",
+  async (punkIndex, minValue, to, { transactionHash }) => {
+    const etherStr = ethers.utils.formatEther(minValue);
+    const reciever = await getAddressName(to);
+    const msg = `PunkOffered: ${punkIndex} was offerd from ${reciever} for ${etherStr} ether.`;
+    sendNotify(msg, transactionHash, punkIndex);
+  }
+);
